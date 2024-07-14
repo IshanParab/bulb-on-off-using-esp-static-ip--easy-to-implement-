de407bf8743ae0ae77904c2efe3fec08b97fdce5
Ishan Parab <174776588+IshanParab@users.noreply.github.com>
2024-07-14 21:16:51 +0530

    v2
    without css and webapp(designed for desktop )
    with static ip : 192.168.0.17
    192.168.0.17 when entered this ip in web browser you should see the server
    it has basix HTML
    replace ssid and password with your own

--- Wifi/Wifi.ino
+++ Wifi/Wifi.ino
@@ -1,41 +1,69 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
-const char* ssid = "ssid(wifiname)";
+
+// Replace with your network credentials
+const char* ssid = "ssid";
 const char* password = "password";
+
+// Static IP address configuration
+IPAddress local_IP(192, 168, 0, 17); // Replace with your desired static IP address
+IPAddress gateway(192, 168, 0, 1);    // Replace with your network gateway
+IPAddress subnet(255, 255, 255, 0);   // Replace with your network subnet mask
+
+// Create an instance of the ESP8266WebServer class
 ESP8266WebServer server(80);
+
+// GPIO pin connected to the LED
+const int ledPin = D7; // Change D7 to the GPIO pin you connected your LED to
 
 void setup() {
   Serial.begin(115200);
-  WiFi.begin(ssid, password);
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW); // Ensure the LED is off initially
 
+  // Configure static IP address
+  if (!WiFi.config(local_IP, gateway, subnet)) {
+    Serial.println("STA Failed to configure");
+  }
+
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting to WiFi...");
   }
+  Serial.println("Connected to WiFi");
 
-  Serial.println("Connected to WiFi!");
-}
-server.on("/", handleRoot);
-server.on("/led/on", handleLedOn);
-server.on("/led/off", handleLedOff);
-server.begin();
-}
-Serial.println("Web server started!");
-void handleRoot() {
-  server.send(200, "text/html", "<h1>Hello from ESP8266!</h1>");
-}
+  // Print ESP8266 local IP address
+  Serial.println(WiFi.localIP());
 
-void handleLedOn() {
-  // Code to turn LED on goes here
-  server.send(200, "text/plain", "LED turned on");
+  // Handle HTTP GET requests
+  server.on("/", HTTP_GET, []() {
+    String html = "<html><body>";
+    html += "<h1 style=\"font-size: 70px;\">Bulb Control</h1>";
+    html += "<p style=\"font-size: 500px;\">Click <a href=\"/led/on\">here</a> to turn bulb on</p>";
+    html += "<p style=\"font-size: 50px;\">Click <a href=\"/led/off\">here</a> to turn bulb off</p>";
+    html += "</body></html>";
+    server.send(200, "text/html", html);
+  });
+
+  // Handle HTTP POST requests to control LED
+  server.on("/led/on", HTTP_GET, []() {
+    digitalWrite(ledPin, HIGH);
+    server.sendHeader("Location", "/");
+    server.send(303); // 303 See Other to redirect to home page
+  });
+
+  server.on("/led/off", HTTP_GET, []() {
+    digitalWrite(ledPin, LOW);
+    server.sendHeader("Location", "/");
+    server.send(303); // 303 See Other to redirect to home page
+  });
+
+  // Start server
+  server.begin();
+  Serial.println("HTTP server started");
 }
-
-void handleLedOff() {
-  // Code to turn LED off goes here
-  server.send(200, "text/plain", "LED turned off");
-}
-
-
 
 void loop() {
   server.handleClient();
